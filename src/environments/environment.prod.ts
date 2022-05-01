1dbbc74f40520c71376bf812547d5c6fe50582f2
isaias calfin <emacalfin@hotmail.com>
2022-05-01 02:22:04 -0300

    firebase products

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firabase:{
+    apiKey: "AIzaSyCcy7SMs5Ws4c-ZQXGdltc6SjtMKmq_XYM",
+    authDomain: "dashboard-c7aa7.firebaseapp.com",
+    projectId: "dashboard-c7aa7",
+    storageBucket: "dashboard-c7aa7.appspot.com",
+    messagingSenderId: "114498034166",
+    appId: "1:114498034166:web:5b0290970829abdfd34fd7"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firabase:{
+    apiKey: "AIzaSyCcy7SMs5Ws4c-ZQXGdltc6SjtMKmq_XYM",
+    authDomain: "dashboard-c7aa7.firebaseapp.com",
+    projectId: "dashboard-c7aa7",
+    storageBucket: "dashboard-c7aa7.appspot.com",
+    messagingSenderId: "114498034166",
+    appId: "1:114498034166:web:5b0290970829abdfd34fd7"
+  }
 };
 
 /*
